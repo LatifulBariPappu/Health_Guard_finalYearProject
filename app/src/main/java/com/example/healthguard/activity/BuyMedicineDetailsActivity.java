b95dc5355af42a5fab279eb577642cb8693d4f98
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2024-04-24 17:29:47 +0600

    buy medicine details activity

--- app/src/main/java/com/example/healthguard/activity/BuyMedicineDetailsActivity.java
+++ app/src/main/java/com/example/healthguard/activity/BuyMedicineDetailsActivity.java
@@ -1,14 +1,34 @@
 package com.example.healthguard.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.healthguard.R;
 
 public class BuyMedicineDetailsActivity extends AppCompatActivity {
-
+    TextView tvPackageName,tvTotalCost;
+    EditText edDetails;
+    Button btnBack,btnAddtoCart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_medicine_details);
+
+        tvPackageName=findViewById(R.id.textViewBMDPackageName);
+        tvTotalCost=findViewById(R.id.textViewBMDTotalCost);
+        edDetails=findViewById(R.id.editTextBMDTextMultiline);
+        edDetails.setKeyListener(null);
+        btnAddtoCart=findViewById(R.id.buttonBMDAddtoCart);
+        btnBack=findViewById(R.id.buttonBMDBack);
+
+        Intent intent=getIntent();
+        tvPackageName.setText(intent.getStringExtra("text1"));
+        edDetails.setText(intent.getStringExtra("text2"));
+        tvTotalCost.setText("Total Cost : "+intent.getStringExtra("text3")+"/-");
+
     }
 }
