8679c8ba4ed590e0971a5eee91ef09c70c316d7a
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2024-05-02 01:36:25 +0600

    ambulance activity fontend

--- app/src/main/java/com/example/healthguard/activity/AmbulanceActivity.java
+++ app/src/main/java/com/example/healthguard/activity/AmbulanceActivity.java
@@ -4,21 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.cardview.widget.CardView;
 
 import com.example.healthguard.R;
 
 public class AmbulanceActivity extends AppCompatActivity {
     Button btnback;
+    CardView amb1,amb2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ambulance);
+        amb1=findViewById(R.id.cardViewAmb1);
+        amb2=findViewById(R.id.cardViewAmb2);
         btnback=findViewById(R.id.backbuttonAmb);
 
         btnback.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +26,17 @@ public class AmbulanceActivity extends AppCompatActivity {
                 startActivity(new Intent(AmbulanceActivity.this, MainActivity.class));
             }
         });
-
+        amb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AmbulanceActivity.this,NormalAmbulanceActivity.class));
+            }
+        });
+        amb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AmbulanceActivity.this,FrezzAmulanceActivity.class));
+            }
+        });
     }
 }
