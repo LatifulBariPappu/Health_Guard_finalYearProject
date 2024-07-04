935300be537a35561f8611ec35ab4d70b40f8718
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2024-07-04 12:59:03 +0600

    sender reciver shape

--- app/src/main/java/com/example/healthguard/activity/ChatDocActivity.java
+++ app/src/main/java/com/example/healthguard/activity/ChatDocActivity.java
@@ -1,27 +1,69 @@
 package com.example.healthguard.activity;
 
-import static com.example.healthguard.activity.LoginActivity.SHARED_PREFS;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.example.healthguard.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ChatDocActivity extends AppCompatActivity {
-    TextView chatUsername;
+    FirebaseAuth auth;
+    RecyclerView mainUserRecyclerView;
+    UserAdpter  adapter;
+    FirebaseDatabase database;
+    ArrayList<Users> usersArrayList;
+    ImageView setbut;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_doc);
 
-        Users users=new Users();
-        String username=users.getUserName();
+        database=FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+        setbut = findViewById(R.id.settingBut);
 
-        chatUsername=findViewById(R.id.chatUserName);
-        chatUsername.setText(username);
+        DatabaseReference reference = database.getReference().child("user");
 
+        usersArrayList = new ArrayList<>();
+
+        mainUserRecyclerView = findViewById(R.id.mainUserRecyclerView);
+        mainUserRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new UserAdpter(ChatDocActivity.this,usersArrayList);
+        mainUserRecyclerView.setAdapter(adapter);
+
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot: snapshot.getChildren())
+                {
+                    Users users = dataSnapshot.getValue(Users.class);
+                    usersArrayList.add(users);
+                }
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
